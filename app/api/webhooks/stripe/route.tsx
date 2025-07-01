37fce61d5d1750a69967d5f9c958eaaddcead7d5
Ivs270191 <152504971+Ivs270191@users.noreply.github.com>
2025-07-01 16:10:17 +0300

    clg2

--- app/api/webhooks/stripe/route.tsx
+++ app/api/webhooks/stripe/route.tsx
@@ -3,43 +3,50 @@ import Stripe from "stripe";
 
 import { sendPurchaseReceipt } from "@/emails";
 import Order from "@/lib/db/models/order.model";
+import { connectToDatabase } from "@/lib/db";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export async function POST(req: NextRequest) {
-  const event = await stripe.webhooks.constructEvent(
-    await req.text(),
-    req.headers.get("stripe-signature") as string,
-    process.env.STRIPE_WEBHOOK_SECRET as string
-  );
-  console.log(event.type);
-  if (event.type === "charge.succeeded") {
-    const charge = event.data.object;
-    const orderId = charge.metadata.orderId;
-    const email = charge.billing_details.email;
-    const pricePaidInCents = charge.amount;
-    const order = await Order.findById(orderId).populate("user", "email");
-    if (order == null) {
-      return new NextResponse("Bad Request", { status: 400 });
-    }
+  try {
+    await connectToDatabase();
+    const event = await stripe.webhooks.constructEvent(
+      await req.text(),
+      req.headers.get("stripe-signature") as string,
+      process.env.STRIPE_WEBHOOK_SECRET as string
+    );
 
-    order.isPaid = true;
-    order.paidAt = new Date();
-    order.paymentResult = {
-      id: event.id,
-      status: "COMPLETED",
-      email_address: email!,
-      pricePaid: (pricePaidInCents / 100).toFixed(2),
-    };
-    await order.save();
-    try {
-      await sendPurchaseReceipt({ order });
-    } catch (err) {
-      console.log("email error", err);
+    if (event.type === "charge.succeeded") {
+      const charge = event.data.object;
+      const orderId = charge.metadata.orderId;
+      const email = charge.billing_details.email;
+      const pricePaidInCents = charge.amount;
+      const order = await Order.findById(orderId).populate("user", "email");
+      if (order == null) {
+        return new NextResponse("Bad Request", { status: 400 });
+      }
+
+      order.isPaid = true;
+      order.paidAt = new Date();
+      order.paymentResult = {
+        id: event.id,
+        status: "COMPLETED",
+        email_address: email!,
+        pricePaid: (pricePaidInCents / 100).toFixed(2),
+      };
+      await order.save();
+      try {
+        await sendPurchaseReceipt({ order });
+      } catch (err) {
+        console.log("email error", err);
+      }
+      return NextResponse.json({
+        message: "updateOrderToPaid was successful",
+      });
     }
-    return NextResponse.json({
-      message: "updateOrderToPaid was successful",
-    });
+    return new NextResponse();
+  } catch (err) {
+    console.error("Stripe webhook error:", err);
+    return new NextResponse("Webhook Error", { status: 500 });
   }
-  return new NextResponse();
 }
