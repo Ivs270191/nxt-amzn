0d4d9e2a846670be4c4a2c02eb9ae8a7721849f2
Ivs270191 <152504971+Ivs270191@users.noreply.github.com>
2025-07-10 11:50:25 +0300

    search page implemented

--- components/shared/header/search.tsx
+++ components/shared/header/search.tsx
@@ -7,10 +7,12 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { getAllCategories } from "@/lib/actions/product.actions";
 import { APP_NAME } from "@/lib/constants";
 import { SearchIcon } from "lucide-react";
-const categories = ["men", "women", "kids", "accessories"];
+
 export default async function Search() {
+  const categories = await getAllCategories();
   return (
     <form action="/search" method="GET" className="flex items-stretch h-9">
       <Select name="category">
